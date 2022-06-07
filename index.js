2997224fe1ede49503c0b9531eb8ef712a31836c
John Hayes <hayesjohn54@yahoo.com>
2022-06-07 12:53:12 -0400

    commit tree 49471e1b9f8c1c42383945f3a1b0135d35796c38

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    let hq = 42;
+    return Math.abs(blockNumber - 42);
+    
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blocksTravelled = Math.abs(start - destination);
+    let feetTravelled = blocksTravelled * 264;
+    return feetTravelled;
+
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTravelled = distanceTravelledInFeet(start, destination);
+    console.log(feetTravelled);
+    
+    if (feetTravelled < 400) {
+        return 0;
+    } else if (feetTravelled < 2000){
+        return (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
